35a626047c0e8c238e8e6534d968f0f9778364a6
manishanegii <maneesha.negi.2000@gmail.com>
2021-02-23 22:48:03 +0530

    first commit

---
+++ pattern2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int n=5;
+    int i,j;
+    for(i=0; i<n; i++){
+        for(j=0; j<n-i-1; j++){
+            printf(" ");
+        }
+       for(j=0; j<=i; j++){
+        printf("*");
+    }
+        printf("\n"); 
+    }
+}
