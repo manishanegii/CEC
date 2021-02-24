e2f6528449f02f0f2afb62eba4c4135b070da6e4
manishanegii <49955919+manishanegii@users.noreply.github.com>
2021-02-24 22:58:36 +0530

    Update pattern5.c

--- pattern5.c
+++ pattern5.c
@@ -4,11 +4,11 @@ int main(){
     int n=5;
     int i,j;
     for(i=0; i<n; i++){
-        for(j=0; j<n-i-1; j++){
+        for(j=0; j<n-i; j++){
             printf(" ");
         }
-       for(j=0; j<i=1; j++){
-        printf("*");
+       for(j=0; j<=i; j++){
+        printf("* ");
     }
         printf("\n"); 
     }
